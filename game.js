e58e2b53760c34616905fa323fa8fb8fcad2c4d6
EC2 Default User <ec2-user@ip-172-31-48-72.us-west-2.compute.internal>
2022-06-26 10:14:23 +0000

    initial commit

---
+++ game.js
@@ -0,0 +1,16 @@
+console.log("javascript loaded")
+
+//game logic, 
+/*
+
+I need to be able to click on a card to select it
+
+With the card selected, I should be able to click a box on the field to set the card
+
+If a card is placed on the field, It will be "used" and unable to be selected until the location is clicked again to remove this card
+
+Once all three spots are chosen, the results of the round will be generated
+
+Fow now, I will ignore building a CPU and use a pregenerated list of rounds.
+
+*/
