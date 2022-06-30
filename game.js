18cec08c429032caebb86619a78cad850a65ba43
EC2 Default User <ec2-user@ip-172-31-48-72.us-west-2.compute.internal>
2022-06-30 22:55:59 +0000

    remove dom interaction in game.js

--- game.js
+++ game.js
@@ -1,16 +1,74 @@
-console.log("javascript loaded")
 
-//game logic, 
-/*
 
-I need to be able to click on a card to select it
+//triangle defeat square defeat circle defeat triangle
 
-With the card selected, I should be able to click a box on the field to set the card
+let createPlayer = (name) => {
+    const cards = 
+    {triangle:4,
+    circle:4,
+    square:4}
+    
+    let field = {left:"empty",right:"empty"}
+    let setField = (location,attack) => {
+        field[location] = attack;
+    }
+    let getField = ()=> {
+        return field;
+    }
+    let getDeck = ()=> {
+        return cards;
+    }
+    return{
+        name:name,
+        cards:cards,
+        field:field,
+        setField:setField,
+    }
+}
 
-If a card is placed on the field, It will be "used" and unable to be selected until the location is clicked again to remove this card
+let compareFields = (homeField,awayField) => {
+    let leftResult = compareCards(homeField.left,awayField.left) 
+    let rightResult = compareCards(homeField.right,awayField.right) 
+    
+    console.log("leftResult",leftResult)
+    console.log("rightResult",rightResult)
+}
 
-Once all three spots are chosen, the results of the round will be generated
+let shapeComparisonObject = {"triangle":"square",
+"square":"circle",
+"circle":"triangle"
+}
+let compareCards = (card1,card2) =>{
+    //check if one card defeats the other
+    if(shapeComparisonObject[card1] == card2){
+        //card 1 beats card2
+        console.log(`${card1} beats ${card2}`)
+        return 1
+    }
+    else if(shapeComparisonObject[card2] == card1){
+        //card 2 beats card1
+        console.log(`${card2} beats ${card1}`)
+        return -1
+    }
+    else if(card1 == card2){
+        //card are equal
+        console.log(`${card1} draws with ${card2}`)
+        return 0
+    }
+    else{
+        //invalid card
+        console.log(`${card1} or ${card2} is an invalid card.
+        Valid cards are :: triangle square and circle`)
+    }
+}
 
-Fow now, I will ignore building a CPU and use a pregenerated list of rounds.
+let michael = createPlayer("michael");
+let opponent = createPlayer("bbeg");
 
-*/
+michael.setField("left","triangle")
+michael.setField("right","square")
+
+opponent.setField("left","square")
+opponent.setField("right","square")
+
+console.log(compareFields(michael.field,opponent.field))
