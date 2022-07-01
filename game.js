bb90d25b9c616f79e9c597f7c9646838f461f750
EC2 Default User <ec2-user@ip-172-31-48-72.us-west-2.compute.internal>
2022-07-01 22:26:53 +0000

    feat game is now playable in the dom

--- game.js
+++ game.js
@@ -69,26 +69,12 @@ const compareCards = (card1,card2) =>{
     }
 }
 
-const promptFieldInput = (player1,player2) => {
-    
-    let player1FullField = prompt(`Player1${player1.name} enter your field input seperated by spaces`)
-    let player2FullField = prompt(`Player2${player2.name} enter your field input seperated by spaces`)
-    
-    // let player1FullField  = "triangle triangle"
-    // let player2FullField = "square squre"
-    
-    player1.field.left = player1FullField.split(" ")[0]
-    player1.field.right = player1FullField.split(" ")[1]
-    
-    player2.field.left = player2FullField.split(" ")[0]
-    player2.field.right = player2FullField.split(" ")[1]
-}
+
 
 const playerBattle = (player1,player2) => {
     //compare the players fields, assign points, then reset.
-    
-    //ask the user for field input
-    promptFieldInput(player1,player2)
+    console.log("playerBattle",player1.field, player2.field)
+
     //compare fields to generate the round object
     let round = compareFields(michael.field,opponent.field)
     
@@ -121,10 +107,7 @@ const clearFields = (player1,player2) => {
 let michael = createPlayer("michael");
 let opponent = createPlayer("bbeg");
 
-
-michael.setField("left","triangle")
-michael.setField("right","square")
-
+//placeholder
 opponent.setField("left","square")
 opponent.setField("right","square")
 
@@ -134,6 +117,7 @@ opponent.setField("right","square")
 
 
 const gameLoop = () => {
+    //runs one round
     let gameResults = {
         rounds:0,
         timeElapsed:0,
@@ -142,19 +126,100 @@ const gameLoop = () => {
         
     }
     
-    console.log("michael.score",michael.getPoints())
-    while (michael.getPoints() < 3 && opponent.getPoints() < 3){
-        //request for input
-        playerBattle(michael,opponent)
-        gameResults.rounds++;
-    }
-
-    console.log("michael",michael)
+    playerBattle(michael,opponent)
+    gameResults.rounds++;
+    
     console.log("michael.score",michael.getPoints())
     gameResults.player1Score = michael.getPoints()
     gameResults.player2Score = opponent.getPoints()
     
+    render();
     return gameResults;
+    
 }
-console.log(gameLoop());
 //need a way to advance the game, add points, go to the next step, and then complete the game 
+let domPlayer1 = document.getElementById("player1")
+let domPlayer1Deck = domPlayer1.getElementsByClassName("cards")[0]
+let domPlayer1Left = domPlayer1.getElementsByClassName("left")[0]
+let domPlayer1Right = domPlayer1.getElementsByClassName("right")[0]
+let domPlayer1Submit = domPlayer1.getElementsByClassName("submit")[0]
+let currentSelectedCard;
+let currentSelectedCardType;
+
+let handleLocationSelection = (e) => {
+    let locationSide = Array.from(e.target.classList).filter(c => c == "left" || c =="right")[0]
+    //remove other classes
+    e.target.classList.remove("triangle","circle","square")
+    //set location type to current selected card type
+    if(!!currentSelectedCardType){
+        e.target.classList.add(currentSelectedCardType)
+        console.log("type",currentSelectedCardType)
+        michael.field[locationSide] = currentSelectedCardType;
+    }
+    else{
+        //if no card is selected let us know
+        console.log("no card selected")
+    }
+    
+    //set selected card to none
+}
+let handleCardSelection = (e) => {
+    currentSelectedCard= e.target;
+    currentSelectedCardType = Array.from(e.target.classList).filter(c => c == "triangle" || c =="circle" || c=="square")[0] //use [-data] instaed
+    
+    //remove selected class from other cards
+    for(let domCard of Array.from(domPlayer1Deck.children)){
+        domCard.classList.remove("selected")
+    }
+    //toggle it on this card
+    currentSelectedCard.classList.toggle("selected")
+}
+let handleSubmit = (left,right) =>  (e) => {
+    e.preventDefault();
+    console.log("submit clicked!",e.target, left,right)
+    let leftCardType = Array.from(left.classList).filter(c => c == "circle" || c=="square" || c == "triangle")[0];
+    let rightCardType = Array.from(right.classList).filter(c => c == "circle" || c=="square" || c == "triangle")[0];
+    // console.log(Array.from(left.classList).filter(c => c == "circle" || c=="square" || c == "triangle"))
+    // console.log(Array.from(right.classList).filter(c => c == "circle" || c=="square" || c == "triangle"))
+    //subtract 1 from player cards object
+    if(michael.cards[leftCardType] > 0 && michael.cards[leftCardType]){
+        michael.cards[leftCardType]--;
+        michael.cards[rightCardType]--;
+        //battle current field vs opponent hand
+        gameLoop();
+    }
+    else{
+        console.log("error Missing either",leftCardType,rightCardType)
+    }
+    //re render the cards
+    
+}
+domPlayer1Left.addEventListener("click",handleLocationSelection)
+domPlayer1Right.addEventListener("click",handleLocationSelection)
+
+domPlayer1Submit.addEventListener("click",handleSubmit(domPlayer1Left,domPlayer1Right))
+
+//generate deck
+let render = () => {
+    
+    //remove all cards
+    while (domPlayer1Deck.firstChild) {
+        domPlayer1Deck.removeChild(domPlayer1Deck.firstChild);
+    }
+    
+    //generate new cards
+    for(let shape in michael.cards){
+    for(let i = 0; i <michael.cards[shape]; i++){
+        let cardDiv = document.createElement("div")
+        //add shape to this card
+        cardDiv.classList.add(shape)
+        
+        cardDiv.classList.add("card")
+        
+        domPlayer1Deck.appendChild(cardDiv)
+        cardDiv.addEventListener("click",handleCardSelection)
+    }
+}
+}
+
+render();
