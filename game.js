7066f1c4e554e3389b28fd8fbc8ababab865ae12
EC2 Default User <ec2-user@ip-172-31-48-72.us-west-2.compute.internal>
2022-07-01 06:49:09 +0000

    modify game.js, now playable with prompt() to 5 points

--- game.js
+++ game.js
@@ -2,68 +2,125 @@
 
 //triangle defeat square defeat circle defeat triangle
 
-let createPlayer = (name) => {
+const createPlayer = (name) => {
+    let points = 0;
     const cards = 
     {triangle:4,
     circle:4,
     square:4}
     
-    let field = {left:"empty",right:"empty"}
-    let setField = (location,attack) => {
+    const field = {left:"empty",right:"empty"}
+    const setField = (location,attack) => {
+        if(cards[attack] <= 0){
+            console.log("card not in deck")
+            return 0;
+        }
         field[location] = attack;
+        cards[attack]--;
     }
-    let getField = ()=> {
-        return field;
+    const addPoints = (amount) => {
+        points = points+amount;
     }
-    let getDeck = ()=> {
-        return cards;
-    }
+    const getPoints = () => points;
     return{
         name:name,
         cards:cards,
         field:field,
+        getPoints:getPoints,
         setField:setField,
+        addPoints:addPoints
     }
 }
 
-let compareFields = (homeField,awayField) => {
+const compareFields = (homeField,awayField) => {
     let leftResult = compareCards(homeField.left,awayField.left) 
     let rightResult = compareCards(homeField.right,awayField.right) 
     
-    console.log("leftResult",leftResult)
-    console.log("rightResult",rightResult)
+    return[leftResult,rightResult]
 }
 
-let shapeComparisonObject = {"triangle":"square",
+const shapeComparisonObject = {"triangle":"square",
 "square":"circle",
 "circle":"triangle"
 }
-let compareCards = (card1,card2) =>{
+
+const compareCards = (card1,card2) =>{
     //check if one card defeats the other
     if(shapeComparisonObject[card1] == card2){
         //card 1 beats card2
-        console.log(`${card1} beats ${card2}`)
+        // console.log(`${card1} beats ${card2}`)
         return 1
     }
     else if(shapeComparisonObject[card2] == card1){
         //card 2 beats card1
-        console.log(`${card2} beats ${card1}`)
+        // console.log(`${card1} loses to ${card2}`)
         return -1
     }
     else if(card1 == card2){
         //card are equal
-        console.log(`${card1} draws with ${card2}`)
+        // console.log(`${card1} draws with ${card2}`)
         return 0
     }
     else{
         //invalid card
         console.log(`${card1} or ${card2} is an invalid card.
         Valid cards are :: triangle square and circle`)
+        return "invalid"
     }
+}
+
+const promptFieldInput = (player1,player2) => {
+    
+    let player1FullField = prompt(`Player1${player1.name} enter your field input seperated by spaces`)
+    let player2FullField = prompt(`Player2${player2.name} enter your field input seperated by spaces`)
+    
+    // let player1FullField  = "triangle triangle"
+    // let player2FullField = "square squre"
+    
+    player1.field.left = player1FullField.split(" ")[0]
+    player1.field.right = player1FullField.split(" ")[1]
+    
+    player2.field.left = player2FullField.split(" ")[0]
+    player2.field.right = player2FullField.split(" ")[1]
+}
+
+const playerBattle = (player1,player2) => {
+    //compare the players fields, assign points, then reset.
+    
+    //ask the user for field input
+    promptFieldInput(player1,player2)
+    //compare fields to generate the round object
+    let round = compareFields(michael.field,opponent.field)
+    
+    //Assign the points
+    for(let locationScore of round){
+        if(locationScore == 1){
+            player1.addPoints(1)
+        }
+        else if(locationScore == 0){
+            
+        }
+        else if(locationScore == -1){
+            player2.addPoints(1)
+        }
+    }
+    // console.log("points", player1.getPoints(), player2.getPoints())
+    // console.log("players",player1,player2)
+    console.log(player1.getPoints(),player2.getPoints())
+    // returns a score
+}
+
+const clearFields = (player1,player2) => {
+    player1.setField("left","empty")
+    player1.setField("right","empty")
+
+    player2.setField("left","empty")
+    player2.setField("right","empty")
 }
 
 let michael = createPlayer("michael");
 let opponent = createPlayer("bbeg");
+
 
 michael.setField("left","triangle")
 michael.setField("right","square")
@@ -71,4 +128,33 @@ michael.setField("right","square")
 opponent.setField("left","square")
 opponent.setField("right","square")
 
-console.log(compareFields(michael.field,opponent.field))
+
+
+// playerBattle(michael,opponent)
+
+
+const gameLoop = () => {
+    let gameResults = {
+        rounds:0,
+        timeElapsed:0,
+        player1Score:0,
+        player2Score:0,
+        
+    }
+    
+    console.log("michael.score",michael.getPoints())
+    while (michael.getPoints() < 3 && opponent.getPoints() < 3){
+        //request for input
+        playerBattle(michael,opponent)
+        gameResults.rounds++;
+    }
+
+    console.log("michael",michael)
+    console.log("michael.score",michael.getPoints())
+    gameResults.player1Score = michael.getPoints()
+    gameResults.player2Score = opponent.getPoints()
+    
+    return gameResults;
+}
+console.log(gameLoop());
+//need a way to advance the game, add points, go to the next step, and then complete the game 
